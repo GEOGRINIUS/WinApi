62df962a2f13cac3e227ba8ba347d41d6b8cc80a
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-06-29 13:23:56 +0400

    Add project files.

---
+++ DialogApplication/Main.cpp
@@ -0,0 +1,26 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+//Прототип функци:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//Вызов функци:
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, NULL);
+}
+
+//Реализация функци:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_CLOSE:
+		break;
+	}
+	return FALSE;
+}
---
+++ WinAPI/Main.cpp
@@ -0,0 +1,13 @@
+﻿#include<Windows.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	MessageBox
+	(
+		NULL, //Родительское окно.
+		"Hello WinAPI!", //сообщение выводимое пользователю.
+		"Hello", //Название окно.
+		//Флаги, которые определяют значек, выводимый в окне, набор кнопок, и некоторый одполнительные параметры.
+		MB_YESNOCANCEL | MB_HELP | MB_ICONQUESTION | MB_DEFBUTTON3 | MB_SYSTEMMODAL
+	);
+}
