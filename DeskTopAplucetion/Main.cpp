91991b554be51fe334ac1a910294379dcf0a8fca
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2026-07-08 13:30:27 +0400

    ComboBox DONE;

--- DeskTopAplucetion/Main.cpp
+++ DeskTopAplucetion/Main.cpp
@@ -1,4 +1,5 @@
 #include<Windows.h>
+#include<cstdio>
 #include"resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "First", "Combo", "Box" };
@@ -27,6 +28,28 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+
+			CONST INT SIZE = 256;
+			CHAR szBuffer[SIZE] = {};
+			CHAR szMessage[SIZE] = {};
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)szBuffer);
+			sprintf
+			(
+				szMessage,
+				"Вы выбрали элеменет №%i со значением '%s'.", 
+				i, szBuffer
+			);
+			MessageBox(hwnd, szMessage, "Info", MB_OK | MB_ICONINFORMATION);
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
